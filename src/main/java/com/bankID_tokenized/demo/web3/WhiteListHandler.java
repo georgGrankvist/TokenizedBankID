742551d1f1870a7befe6d363d971ad27b9da8d15
georgGrankvist <62744506+georgGrankvist@users.noreply.github.com>
2022-03-09 13:42:36 +0100

    goerli redeployment

--- src/main/java/com/bankID_tokenized/demo/web3/WhiteListHandler.java
+++ src/main/java/com/bankID_tokenized/demo/web3/WhiteListHandler.java
@@ -13,9 +13,14 @@ import org.web3j.tx.RawTransactionManager;
 import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.tx.gas.StaticGasProvider;
+import org.web3j.tx.response.Callback;
+import org.web3j.tx.response.PollingTransactionReceiptProcessor;
+import org.web3j.tx.response.QueuingTransactionReceiptProcessor;
+import org.web3j.tx.response.TransactionReceiptProcessor;
 import wrapper.TokenizedBankID;
 
 import java.math.BigInteger;
+import java.net.URI;
 import java.util.concurrent.CompletableFuture;
 
 @Getter
@@ -33,8 +38,8 @@ public class WhiteListHandler {
     }
 
     public WhiteListHandler () {
-        web3j = Web3j.build(new HttpService("https://polygon-mumbai.infura.io/v3/ee8a83a7efef41bb88178cf740c69511"));
-        walletCredentials = Credentials.create("");
+        web3j = Web3j.build(new HttpService("https://goerli.infura.io/v3/ee8a83a7efef41bb88178cf740c69511"));
+        walletCredentials = Credentials.create("fe559c7814450d6f6c9a635c0d88afa70244598342be87a249f0c9fa3bfc83c7");
     }
 
     public void web3Connect (String address) {
@@ -49,16 +54,18 @@ public class WhiteListHandler {
             System.out.println("Gas price: " + gasPrice.getGasPrice());
             System.out.println("Address to be Whitelisted: " + address);
 
-            TransactionManager transactionManager = new FastRawTransactionManager(web3j,walletCredentials, 80001);
+            TransactionReceiptProcessor transactionReceiptProcessor = new PollingTransactionReceiptProcessor(web3j,3000,40);
+            TransactionManager transactionManager = new FastRawTransactionManager(web3j,walletCredentials,transactionReceiptProcessor);
+            TokenizedBankID whiteListContract = TokenizedBankID.load("0xd655da810B2a1304eBB6707f9396C5c2138819c4",web3j, transactionManager,new DefaultGasProvider());
+            CompletableFuture<TransactionReceipt> transactionReceipt = whiteListContract.addUser(address).sendAsync();
 
+            transactionReceiptProcessor.waitForTransactionReceipt(transactionReceipt.get().getTransactionHash());
 
-            TokenizedBankID whiteListContract = TokenizedBankID.load("0xb493914c7b574efdf53dee89a824b7347c0d8c09",web3j, transactionManager,new DefaultGasProvider());
-            TransactionReceipt transactionReceipt = whiteListContract.addUser( address ).send();
-
-                System.out.println(transactionReceipt.getTransactionHash());
-                System.out.println("Address: " + address + " is Whitelisted");
-                System.out.println(transactionReceipt.getStatus());
-                System.out.println(transactionReceipt.getGasUsed());
+            if (transactionReceipt.isDone()) {
+                System.out.println("Whitelisting successful, TX HASH = " + transactionReceipt.get().getTransactionHash());
+                System.out.println("Address " + address + " is whitelisted.");
+                System.out.println("TX on block explorer: "  + URI.create("https://goerli.etherscan.io/tx/") + transactionReceipt.get().getTransactionHash());
+            }
 
 
         } catch (Exception e) {
